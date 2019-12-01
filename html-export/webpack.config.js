e641aa539976c4724bf0af1ea0a302c561d7a84f
tycho <tycho.kirchner@fh-erfurt.de>
2019-12-01 20:50:46 +0100

    Add licenses to the html-plot

--- html-export/webpack.config.js
+++ html-export/webpack.config.js
@@ -1,6 +1,11 @@
+const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
+
 module.exports = {
   optimization:{
       minimize: false, // maybe_todo: set to true for release
   },
+  plugins: [
+    new LicenseWebpackPlugin()
+  ],
   mode:'production',
 }
