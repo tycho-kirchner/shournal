e3401132e7270c218f708d5b8d0977729e7f0c19
tycho <tychokirchner@mail.de>
2022-09-19 18:05:30 +0200

    Bump version to 2.9. Add compatibility-map to allow running older versions of shournal's kernel module, if possible.

--- src/shournal-run/mark_helper.cpp
+++ src/shournal-run/mark_helper.cpp
@@ -1,7 +1,8 @@
 #include "mark_helper.h"
 
 #include <sys/user.h>
-
+#include <QHash>
+#include <QVersionNumber>
 
 #include "app.h"
 #include "shournalk_ctrl.h"
@@ -9,6 +10,17 @@
 #include "translation.h"
 #include "os.h"
 
+using std::unordered_map;
+using std::string;
+
+// To avoid unnecessary unloading of the kernel module shournalk
+// when a new version is installed,
+// define a compatibility map, where the key corresponds
+// to the "current" version and the value determines the *minimum*
+// required version.
+static const QHash<QVersionNumber, QVersionNumber> KVERSION_COMPATIBILITY_MAP = {
+    {QVersionNumber{2,9}, QVersionNumber{2,8}}
+};
 
 ExcShournalk::ExcShournalk(const QString &text) :
     QExcCommon(text, false)
@@ -58,11 +70,16 @@ ShournalkControl::ShournalkControl()
                            .arg(translation::strerror_l(errno)));
     }
     if(strcmp(SHOURNAL_VERSION, kversion.ver_str) != 0){
-        throw ExcShournalk(qtr("Version mismatch - %1 version is %2, but "
-                               "%3 version is %4")
-                           .arg(app::SHOURNAL_RUN).arg(SHOURNAL_VERSION)
-                           .arg("kernel-module").arg(kversion.ver_str)
-                           );
+        auto kver = QVersionNumber::fromString(kversion.ver_str);
+        const auto & compatible_ver = KVERSION_COMPATIBILITY_MAP.find(app::version());
+        if(compatible_ver == KVERSION_COMPATIBILITY_MAP.end() ||
+                kver < compatible_ver.value()){
+            throw ExcShournalk(qtr("Version mismatch - %1 version is %2, but "
+                                   "%3 version is %4")
+                               .arg(app::SHOURNAL_RUN).arg(SHOURNAL_VERSION)
+                               .arg("kernel-module").arg(kversion.ver_str)
+                               );
+        }
     }
 
     m_tmpFileTarget = stdiocpp::tmpfile(O_NOATIME); // tmpfile auto deletes..
